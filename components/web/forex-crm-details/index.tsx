464cdf02c0987a25680f1da320703866510cb3ad
abdulmateen393rejoice <abdulmateen393.rejoice@gmail.com>
2026-04-27 18:16:15 +0530

    Added common component for header

--- components/web/forex-crm-details/index.tsx
+++ components/web/forex-crm-details/index.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import ForexCrmDetailsHero from './forex-crm-header'
 import ForexCrmDetailsView from './forex-crm-details-view'
-
+import BrokerDetailsHero from '../broker-details/broker-details-hero'
 export default async function ForexCrmDetails({ broker, randomBrokers = [], trustedBrokers = [] }: { broker: any, randomBrokers?: any[], trustedBrokers?: any[] }) {
     console.log("Crm broker", broker)
     return (
         <div>
-            <ForexCrmDetailsHero broker={broker} />
+            {/* <ForexCrmDetailsHero broker={broker} /> */}
+            <BrokerDetailsHero broker={broker} heroFeatures={[
+                { value: broker.deployment_type || '-', label: 'Deployment' },
+                { value: broker.starting_price || '-', label: 'Starting Price' },
+                { value: broker.bestFor?.join(', ') || '-', label: 'Best For' },
+                { value: broker.demoAccount ? 'Yes' : 'No', label: 'Demo Account' }
+            ]} showDemo={true} showVerified={true} showCrmPara={true}/>
             {/* <BrokerDetailsView broker={broker} randomBrokers={randomBrokers} trustedBrokers={trustedBrokers} /> */}
             <ForexCrmDetailsView broker={broker} randomBrokers={randomBrokers} trustedBrokers={trustedBrokers} />
         </div>
