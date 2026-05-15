72c227de63aec3fa8f150f96dac9203a0ccc2a87
abdulmateen393rejoice <abdulmateen393.rejoice@gmail.com>
2026-05-15 14:04:37 +0530

    Added name to user review

--- app/(web)/tags/[slug]/page.tsx
+++ app/(web)/tags/[slug]/page.tsx
@@ -64,7 +64,7 @@ export default async function (props: Props) {
     <>
       {/* <Breadcrumbs items={breadcrumbs} /> */}
       <CommonBanner highlightedText="Browse Forex " title=" Broker Tags" image="/assets/images/TagsBanner.png" description="Discover trusted forex brokers through categorized trading features including spreads, platforms, regulations, account types, and market instruments."/>
-      <div className="pt-[140px] pb-100 max-mobile:pb-16 max-mobile:pt-[120px]">
+      <div className=" pb-100 max-mobile:pb-16 max-mobile:pt-[120px]">
         <div className="max-w-[1640px] px-5 max-laptop:px-16 mx-auto relative max-tab:px-5 max-mobile:px-4">
          
 
